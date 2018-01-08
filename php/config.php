3636097d4a07e81f403d1072f3f45aa740a4ed39
Cruz <andrscyv@gmail.com>
2018-01-08 12:43:15 -0600

    Cancele revert

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-$funcionesRegistradas = array("alumnos", "logout", "alumno_id", "alumno_cu", "insertaAlumno");
+$funcionesRegistradas = array("alumnos", "logout", "alumno_id", "alumno_cu", "insertaAlumno","nuevoUsuario");
 
 $bdConfig = array( "servername" => "localhost",
 					"username" => "root",
