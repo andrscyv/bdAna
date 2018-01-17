201e77b0cf0cff80fb821a35694fe5dafabcf909
Cruz <andrscyv@gmail.com>
2018-01-17 12:41:08 -0600

    Inserta actividad funciona

--- php/config.php
+++ php/config.php
@@ -4,11 +4,13 @@
 $funcionesRegistradas = array(
 					"alumnos" => array("alumnos", "alumno_id", "alumno_cu", "insertaAlumno"),
 					"auth" => array("logout", "nuevoUsuario"),
-					"authPublico" => array("auth","tengoSesion")
+					"authPublico" => array("auth","tengoSesion"),
+					"actExtra" => array("insertaActividad")
 				);
 $rutasRegistradas = array("alumnos" => "controladores/ctrl_alumnos.php",
 						  "auth" => "auth.php",
-						  "authPublico" => "auth.php");
+						  "authPublico" => "auth.php",
+							"actExtra" => "controladores/ctrl_actividadesExtra.php");
 
 $bdConfig = array( "servername" => "localhost",
 					"username" => "root",
---
+++ php/controladores/ctrl_actividadesExtra.php
@@ -0,0 +1,46 @@
+<?php 
+
+
+
+function insertaActividad(){
+	global $msql;
+	$conn = $msql->conn;
+	
+	try{
+		if( issetArrPost( $params ) ){
+
+			$stmt = $msql->sqlPrepPost("SELECT * from alumnos where cu = :cuAlum", 
+									array("cuAlum"));
+			$stmt->execute();
+
+			if($stmt->rowCount() > 0){
+				$alum = $stmt->fetch(PDO::FETCH_ASSOC);
+				$_POST["idAlum"] = $alum["idAlum"];
+				$params = array("nombre", "tipo", "idAlum");
+				$stmt = $msql->sqlPrepPost("INSERT INTO actividadesExtra(idAlum, nombre, tipo)
+						VALUES (:idAlum, :nombre, :tipo)", $params);
+
+				$stmt->execute();
+				$idAct = $conn->lastInsertId();
+				$res = json(array("idActividad" => $idAct));
+
+			}
+			else
+				$res = jsonErr("Alumno no existente");
+
+		}
+		else 
+			$res = jsonErr("Error en parametros");
+	}
+	catch(PDOException $e){
+		//$res = jsonErr($e->getMessage());
+		$res = $e;
+	}
+
+	echo $res;
+}
+
+
+
+
+ ?>
